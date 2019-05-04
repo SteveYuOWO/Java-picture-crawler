2a9201564cbc098323278cd888b42491510243ec
LittlePage <littlepageprogram@outlook.com>
2019-05-04 16:52:03 +0800

    This is a Java picture crawler! ver 1.0

---
+++ Worm.java
@@ -0,0 +1,104 @@
+﻿package com.littlepage.getImage;
+
+import java.io.B*;
+import java.net.*;
+import java.util.*;
+import java.util.regex.*;
+
+import javax.swing.JFrame;
+
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.*;
+
+public class Worm{
+	public static int doDownLoad(String url){
+		int successNum=0;
+		try{
+			// 1.get html file=
+			String file = "img";
+			WebClient mWebClient = new WebClient();
+			HtmlPage mHtmlPage = mWebClient.getPage(url);
+			String writeStr = mHtmlPage.asXml();// 文本
+			Pattern pattern = Pattern.compile("http.+?(jpg|png|gif)");
+			Matcher matcher = pattern.matcher(writeStr);
+			ArrayList<String> arrStr = new ArrayList<String>();
+			int i = 0;
+			String fileRanStr=getRandomString(5);
+			while (matcher.find()) {
+				String temp = matcher.group();// 圖片地址
+				System.out.println(temp);
+				if (temp.endsWith("jpg"))
+					if(downloadImage(temp, file + "\\"+fileRanStr + i + ".jpg")==true) successNum++;
+				if (temp.endsWith("png"))
+					if(downloadImage(temp, file + "\\"+fileRanStr + i + ".png")==true) successNum++;
+				if (temp.endsWith("gif"))
+					if(downloadImage(temp, file + "\\"+fileRanStr + i + ".gif")==true) successNum++;
+				i++;
+			}
+			mWebClient.close();
+		}catch(Exception e){
+			System.out.println("没事，小异常不搭界");
+		}
+		return successNum;
+	}
+	/**
+	 * random string
+	 */
+	private static String string = "abcdefghijklmnopqrstuvwxyz";
+	private static int getRandom(int count) {
+	   return (int)Math.round(Math.random()*(count));
+	}
+	private static String getRandomString(int length) {
+		StringBuffer sb = new StringBuffer();
+		int len = string.length();
+		for (int i = 0; i < length; i++) {
+			sb.append(string.charAt(getRandom(len - 1)));
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * getPic
+	 * 
+	 * @param urlString
+	 *            url address
+	 * @param path
+	 *            file path
+	 */
+	public static boolean downloadImage(String urlString, String path) {
+		try {
+			URL url = new URL(urlString);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setConnectTimeout(5 * 1000);
+			InputStream inStream = conn.getInputStream();
+			byte[] data = readInputStream(inStream);
+			File imageFile = new File(path);
+			FileOutputStream outStream = new FileOutputStream(imageFile);
+			outStream.write(data);
+			outStream.close();
+		} catch (Exception e) {
+			System.out.println("下载失败");
+			return false;
+		}
+		System.out.println("下载成功");
+		return true;
+	}
+
+	public static byte[] readInputStream(InputStream inStream) {
+		try {
+			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+			byte[] buffer = new byte[1024];
+			int len = 0;
+			while ((len = inStream.read(buffer)) != -1) {
+				outStream.write(buffer, 0, len);
+			}
+			inStream.close();
+			return outStream.toByteArray();
+		} catch (Exception e) {
+			System.out.println("读取失败");
+		}
+		System.out.println("读取成功");
+		return null;
+	}
+}
---
+++ WormWindow.java
@@ -0,0 +1,64 @@
+﻿package com.littlepage.getImage;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class WormWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField jtfUrl;
+
+	public WormWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(2,2));
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		JLabel jl = new JLabel("请输入网址");
+		jl.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(jl);
+		
+		jtfUrl = new JTextField();
+		panel.add(jtfUrl);
+		jtfUrl.setColumns(10);
+		
+		JButton jbConfirm = new JButton("确认");
+		panel.add(jbConfirm);
+		
+		JButton jbCancel = new JButton("取消");
+		panel.add(jbCancel);
+		setSize(400,120);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		jbConfirm.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String url=jtfUrl.getText();
+				int num=Worm.doDownLoad(url);
+				if(num!=0) JOptionPane.showMessageDialog(null, "下载成功"+num+"条");
+				else JOptionPane.showMessageDialog(null, "下载失败");
+			}
+		});
+		jbCancel.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		setTitle("WromPic V1.0");
+		setIconImage(new ImageIcon("icon/icon.jpg").getImage());
+	}
+	public static void main(String[] args) {
+		new WormWindow();
+	}
+}
